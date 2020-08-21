a373af5ef6393ca7ff67e8e2783217ae835b6f44
nouhana <nouhana.elg@gmail.com>
2020-08-21 07:58:41 -0700

    Create prune-old-php-apache-images.sh

---
+++ dockerstack/prune-old-php-apache-images.sh
@@ -0,0 +1,4 @@
+REGISTRY=dockerprivatereg
+REPOSITORY=dockerstack_php_apache
+KEEP=10
+diff <(az acr repository show-tags -n ${REGISTRY} --repository ${REPOSITORY} --orderby time_desc --top ${KEEP} -o tsv) <(az acr repository show-tags -n ${REGISTRY} --repository ${REPOSITORY} --orderby time_desc -o tsv) | awk '{ print $2 }' | xargs -n 1 -I%  az acr repository delete -n ${REGISTRY}  --image ${REPOSITORY}:% --yes
